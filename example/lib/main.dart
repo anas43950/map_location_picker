bb5904602489bd778c4885e9cbba5a4efcd26223
Anas Ansari <anas43950@gmail.com>
2024-12-14 12:55:30 +0530

    removed testing data

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -123,46 +123,6 @@ class _MyAppState extends State<MyApp> {
                   MaterialPageRoute(
                     builder: (context) {
                       return MapLocationPicker(
-                        polygons: {
-                          Polygon(
-                            fillColor: Colors.red.shade100,
-                            polygonId: const PolygonId("1"),
-                            points: const [
-                              LatLng(-73.9745092, 40.6093592),
-                              LatLng(-73.9733183, 40.6043658),
-                              LatLng(-73.9737582, 40.6016781),
-                              LatLng(-73.9739084, 40.5991117),
-                              LatLng(-73.972975, 40.5992176),
-                              LatLng(-73.9734364, 40.5952503),
-                              LatLng(-73.9710438, 40.5949977),
-                              LatLng(-73.970679, 40.5926759),
-                              LatLng(-73.9701748, 40.5901014),
-                              LatLng(-73.9494467, 40.5924641),
-                              LatLng(-73.9498329, 40.5945008),
-                              LatLng(-73.9401555, 40.5955599),
-                              LatLng(-73.9410138, 40.6000404),
-                              LatLng(-73.9357996, 40.6006269),
-                              LatLng(-73.9345765, 40.6019791),
-                              LatLng(-73.9334236, 40.6027286),
-                              LatLng(-73.9206268, 40.6065388),
-                              LatLng(-73.9195562, 40.6071475),
-                              LatLng(-73.9345336, 40.620321),
-                              LatLng(-73.9454985, 40.6308581),
-                              LatLng(-73.9570802, 40.6295919),
-                              LatLng(-73.9656283, 40.6285922),
-                              LatLng(-73.9653357, 40.6270712),
-                              LatLng(-73.9705606, 40.6264763),
-                              LatLng(-73.9697636, 40.6222906),
-                              LatLng(-73.9733293, 40.622596),
-                              LatLng(-73.9759869, 40.6196684),
-                              LatLng(-73.9742088, 40.6094444),
-                              LatLng(-73.9745092, 40.6093592)
-                            ],
-                          )
-                        },
-                        urlModifier: (String url) {
-                          return "Hello $url";
-                        },
                         apiKey: API_KEY,
                         popOnNextButtonTaped: true,
                         currentLatLng: const LatLng(-73.9401555, 40.5955599),
